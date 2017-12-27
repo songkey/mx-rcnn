816b65b0650939e9b480a098e1a67a972ecdbc9b
songkey-zy-server <songkey@outlook.com>
2017-12-27 12:31:50 +0800

    change class num

--- rcnn/config.py
+++ rcnn/config.py
@@ -12,7 +12,7 @@ config.FIXED_PARAMS = ['conv1', 'conv2']
 config.FIXED_PARAMS_SHARED = ['conv1', 'conv2', 'conv3', 'conv4', 'conv5']
 
 # dataset related params
-config.NUM_CLASSES = 21
+config.NUM_CLASSES = 2
 config.SCALES = [(512, 1024)]  # first is scale (the shorter side); second is max size
 config.ANCHOR_SCALES = (8, 16, 32)
 config.ANCHOR_RATIOS = (0.5, 1, 2)
